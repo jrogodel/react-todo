b9f242f3de8eebd211ae8a6348049b270461a056
Jamie Godel <jamiegodel@yahoo.com>
2016-07-13 20:14:41 -0400

    Create first components that handle state and props

---
+++ src/components/textDisplay/TextDisplay.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import TextInput from '../textInput/TextInput'
+
+class TextDisplay extends Component {
+
+	render() {
+		return <div>Hey, I'm displaying text: {this.props.text}</div>
+	}
+}
+
+export default TextDisplay
