6f8d86513c1f31790d012a79f9b175f1216ca5d9
Jamie Godel <jamiegodel@yahoo.com>
2016-07-23 21:28:03 -0400

    Update server config

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
+var chokidar = require('chokidar');
+var config = require('./webpack.config.js');
 var express = require('express');
 var path = require('path');
-var config = require('../webpack.config.js');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 
+var compiler = webpack(config);
 var app = express();
 
-var compiler = webpack(config);
-
-app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+// Serve hot-reloading bundle to client
+app.use(webpackDevMiddleware(compiler, {
+	noInfo: true, publicPath: config.output.publicPath
+}));
 app.use(webpackHotMiddleware(compiler));
 
-app.use(express.static('./dist'));
-
-app.use('/', function (req, res) {
-    res.sendFile(path.resolve('client/index.html'));
+// Include server routes as a middleware
+app.use(function(req, res, next) {
+  require('./server/app')(req, res, next);
 });
 
-var port = 3000;
+app.use(express.static('./build'));
 
-app.listen(port, function(error) {
-  if (error) throw error;
-  console.log("Express server listening on port", port);
+app.use(function (req, res) {
+    res.sendFile(path.resolve('./index.html'));
+});
+
+// Do "hot-reloading" of express stuff on the server
+// Throw away cached modules and re-require next time
+// Ensure there's no important state in there!
+const watcher = chokidar.watch('./server');
+
+watcher.on('ready', function() {
+  watcher.on('all', function() {
+    console.log("Clearing /server/ module cache from server");
+    Object.keys(require.cache).forEach(function(id) {
+      if (/[\/\\]server[\/\\]/.test(id)) delete require.cache[id];
+    });
+  });
+});
+
+// Do "hot-reloading" of react stuff on the server
+// Throw away the cached client modules and let them be re-required next time
+compiler.plugin('done', function() {
+  console.log("Clearing /client/ module cache from server");
+  Object.keys(require.cache).forEach(function(id) {
+    if (/[\/\\]client[\/\\]/.test(id)) delete require.cache[id];
+  });
+});
+
+// Configure server address
+const server = http.createServer(app);
+server.listen(3000, 'localhost', function(err) {
+  if (err) throw err;
+
+  const addr = server.address();
+
+  console.log('Listening at http://%s:%d', addr.address, addr.port);
 });
