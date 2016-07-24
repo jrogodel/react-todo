5f8ede26a4f6df8713887287d1762cf5ddcdf0af
Jamie Godel <jamiegodel@yahoo.com>
2016-07-24 00:04:26 -0400

    Revert back to simple project

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,31 @@
 var webpack = require('webpack');
-var path = require('path');
-var qs = require('querystring');
-var autoprefixer = require('autoprefixer');
-
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
-  devtool: '#eval-source-map',
+  devtool: 'inline-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './client/app.js'
+    './client/client.js'
   ],
   output: {
-    path: '__dirname',
-    filename: path.join(__dirname, './build'),
-    publicPath: '/build'
+    path: require("path").resolve("./dist"),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
-  resolve: {
-    extensions: ['', '.js'],
-    alias: {
-      request: 'browser-request'
-    }
-  },
   module: {
-     loaders: [
-       {
-         test: /\.js$/,
-         loader: 'babel',
-         include: path.join(__dirname, 'client'),
-         query: {
-           "env": {
-             "development": {
-               "presets": ["react-hmre"],
-               "plugins": [
-                 ["react-transform", {
-                   "transforms": [{
-                     "transform": "react-transform-hmr",
-                     "imports": ["react"],
-                     "locals": ["module"]
-                   }]
-                 }]
-               ]
-             }
-           },
-         }
-       },
-       {
-         test: /\.css$/,
-         include: path.join(__dirname, 'client'),
-         loader: 'style-loader!css-loader?' + qs.stringify({
-           modules: true,
-           importLoaders: 1,
-           localIdentName: '[path][name]-[local]'
-        })
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'react-hmre']
+        }
       }
     ]
   }
-};
+}
