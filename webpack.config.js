1b1180a23b1e93a2667b4c4c27575e6f296fd516
Jamie Godel <jamiegodel@yahoo.com>
2016-07-23 22:26:27 -0400

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
-const webpack = require('webpack')
-const path = require('path')
-const autoprefixer = require('autoprefixer')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+var webpack = require('webpack');
+var path = require('path');
+var qs = require('querystring');
+var autoprefixer = require('autoprefixer');
 
-// SASS LOADERS
-const sassLoaders = [
-  'css-loader',
-  'postcss-loader',
-  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, '/.client')
-]
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const config = {
-  
-  // INPUT
-  entry: {
-    app: 'value', ['./src/index.js','webpack-hot-middleware/client', 'webpack-dev-server/client']
-  },
-
-  // LOADERS
-  module: {
-    loaders: [
-      {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {presets: ['react', 'es2015', 'react-hmre']},
-      },
-      {
-        test: /\.sass$/,
-        loader:ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
-      }
-    ]
-  },
-
-  // OUTPUT
+module.exports = {
+  devtool: '#eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/app.js'
+  ],
   output: {
-    path: '[name].js',
-    filename: path.join(__dirname, './build')
+    path: '__dirname',
+    filename: path.join(__dirname, './build'),
     publicPath: '/build'
   },
-
-  // ES2015 SOURCE MAPS
-  devtool: {'source-map'},
-
-  // PLUGINS
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-    new ExtractTextPlugin('[name].css')
   ],
-  postcss:
-
-  // RESOLVE
-  resolve: { 
-    extensions: ['', 'js', 'sass'],
-    root: [path.join(__dirname, './client')]
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      request: 'browser-request'
+    }
+  },
+  module: {
+     loaders: [
+       {
+         test: /\.js$/,
+         loader: 'babel',
+         include: path.join(__dirname, 'client'),
+         query: {
+           "env": {
+             "development": {
+               "presets": ["react-hmre"],
+               "plugins": [
+                 ["react-transform", {
+                   "transforms": [{
+                     "transform": "react-transform-hmr",
+                     "imports": ["react"],
+                     "locals": ["module"]
+                   }]
+                 }]
+               ]
+             }
+           },
+         }
+       },
+       {
+         test: /\.css$/,
+         include: path.join(__dirname, 'client'),
+         loader: 'style-loader!css-loader?' + qs.stringify({
+           modules: true,
+           importLoaders: 1,
+           localIdentName: '[path][name]-[local]'
+        })
+      }
+    ]
   }
-}
-
-module.exports = config
+};
