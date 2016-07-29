e89ab41420544a11e688e14519c04a03e86fc4df
Jamie Godel <jamiegodel@yahoo.com>
2016-07-29 16:49:03 -0400

    Complete Reset

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,34 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool:'eval',
+  devtool:'inline-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3030',
-    'webpack/hot/only-dev-server',
+
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: ''
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+    ],
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loaders: ['react-hot', 'babel']
-    }
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
     {
       test: /\.styl/,
       loader: 'style-loader!css-loader!stylus-loader'
     }
     ]
-  },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
-    ]
-};
+  }
+}
